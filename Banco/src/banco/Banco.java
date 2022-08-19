3f91579e26b1990778982e80215f626b02252ed2
cuenta003 <cuenta003@gmail.com>
2022-08-18 23:57:14 -0600

    v1
    v1

---
+++ Banco/src/banco/Banco.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package banco;
+
+/**
+ *
+ * @author adriangarcia
+ */
+public class Banco {
+    
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        banco.Login login = new banco.Login();
+        login.show();
+    }
+    
+}
---
+++ Banco/src/banco/DatosCliente.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package banco;
+
+/**
+ *
+ * @author adriangarcia
+ */
+public class DatosCliente {
+
+    public String CUI;
+    public String Nombre;
+    public String Apellido;
+    
+    DatosCliente( String cui, String nombre, String apellido){
+        this.CUI = cui;
+        this.Nombre = nombre;
+        this.Apellido = apellido;
+    }
+    
+}
